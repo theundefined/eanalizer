cdac8622a9e88aff1ab04831734607ccc430bbf8
Andrzej Dopierała <undefine@aramin.net>
2025-12-12 23:29:07 +0100

    chore: bump version to 0.1.1

--- run_downloader.py
+++
@@ -1,5 +0,0 @@
-# run_downloader.py
-from eanalizer.downloader_cli import main
-
-if __name__ == "__main__":
-    main()
--- run_eanalizer.py
+++
@@ -1,5 +0,0 @@
-# run_eanalizer.py
-from eanalizer.cli import main
-
-if __name__ == "__main__":
-    main()
